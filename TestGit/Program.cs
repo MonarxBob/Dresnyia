af155e0edb49a4744eda90959bb11f4e7f0a55b9
Monarx Af <tch.kacto@yandex.ru>
2026-04-15 19:08:24 +0200

    хелло ворд

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -9,6 +9,9 @@
             int a = 5;
             int b = 11;
             int c = a + b;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Worlааd!");
+
         }
     }
 }
