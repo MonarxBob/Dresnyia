f8885bbbb2a22f317440dd8c5c62a391cef48a1f
Monarx Af <tch.kacto@yandex.ru>
2026-04-15 19:33:47 +0200

    hyita

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -9,9 +9,14 @@
             int a = 5;
             int b = 11;
             int c = a + b;
+            Console.WriteLine(c);
             Console.WriteLine("Hello, WoГИТХАБНАСРАНОrld!");
             Console.WriteLine("Hello, Worlааd!");
-
+            Console.WriteLine("Hello, DOBAVIL XUITY!");
+            int ab = 51;
+            int ba = 21;
+            int cc = ab + ba;
+            Console.WriteLine(cc);
         }
     }
 }
