5eef22a0c8d95bd84e832f6f71638b925441c39b
Monarx Af <tch.kacto@yandex.ru>
2026-04-15 18:59:48 +0200

    Насрал хуйниДобавьте файлы проекта.

---
+++ TestGit/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("ААБАБУЙБКЕБ");
+            int a = 5;
+            int b = 11;
+            int c = a + b;
+        }
+    }
+}
